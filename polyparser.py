30a8e084d101e4d9b65f1e3748108aa4397d654c
CheewyOFF <cheewy.perso@gmail.com>
2023-12-22 10:16:48 +0100

    wip

--- polyparser.py
+++ polyparser.py
@@ -4,6 +4,9 @@
 """Module de parsing des fichiers d'entrée pour la mise en oeuvre du projet Poly#.
 """
 
+from utils.Warehouse import Warehouse
+from utils.Order import Order
+
 def parse_challenge(filename: str) -> object:
     """Lit un fichier de challenge et extrait les informations nécessaires.
 
@@ -11,9 +14,23 @@ def parse_challenge(filename: str) -> object:
     représenter votre challenge: dictionnaire, objet, etc
     """
     with open(filename, 'r') as f:
-        # Lire la première ligne et la découper
-        rows, columns, drone_count, deadline, max_load = [ int(v)
-                                                           for v in f.readline().split() ]
-        # ...
+        rows, columns, drone_count, deadline, max_load = [int(v) for v in f.readline().split()]
+        product_count = int(f.readline())
+        product_weights = [weight for weight in f.readline().split()]
+
+        warehouse_count = int(f.readline())
+        warehouse_list = []
+
+        for _ in range(warehouse_count):
+            x, y = [int(v) for v in f.readline().split()]
+            warehouse_products = [int(v) for v in f.readline().split()]
+            warehouse_list.append(Warehouse(x, y, warehouse_products))
+
+        order_count = int(f.readline())
+        order_list = []
+
+        for _ in range(order_count):
+            order_list.append()
+
 
     return challenge
