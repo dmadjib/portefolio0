d19df51394a7432d387411b5f6b27e9a7f2f51ea
Ahmed Fakhfakh <ahmed.fakhfakh@etu.univ-nantes.fr>
2024-01-16 10:22:35 +0100

    Commenter la classe Warehouse

--- utils/Warehouse.py
+++ utils/Warehouse.py
@@ -1,4 +1,21 @@
+"""
+@title : Warehouse
+@description : Class defining what a warehouse is
+@author : El ARAJNA Lina , FAKHFAKH Ahmed , LALANNE-TISNE Nino, Madjibaye Donatien
+@date : Last Modification : 23/12/2023
+"""
+
+
 class Warehouse:
+    """
+    Class is defined by:
+        - id
+        - x
+        - y
+        - products
+    """
+
+    """ Constructor """
 
     def __init__(self, id, x, y, products) -> None:
         self.id = id
