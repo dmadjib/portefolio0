469762acb46b9420defc898223f3ba06a357b6a0
CheewyOFF <cheewy.perso@gmail.com>
2024-01-16 11:52:38 +0100

    Suppression de méthodes inutiles

--- utils/Drone.py
+++ utils/Drone.py
@@ -5,11 +5,7 @@ class Drone:
         self.max_payload = max_payload
         self.current_location = initial_location
         self.current_load = 0
-        self.available_turn = 0
         self.products = {}
-
-    def available(self, current_turn):
-        return current_turn >= self.available_turn
     
     def can_load(self, product_type, quantity, product_weights):
         incoming_weight = quantity * int(product_weights[product_type])
@@ -20,10 +16,6 @@ class Drone:
     
     def has_product_asked(self, product, quantity):
         return product in self.products and self.products[product] >= quantity
-
-    def fly_to(self, location):
-        distance = Drone.calculate_distance(self.current_location, location)
-        self.available_turn += int(math.ceil(distance))
     
     def load(self, warehouse, product_type, quantity, product_weights, solutions):
         total_weight = quantity * int(product_weights[product_type])
