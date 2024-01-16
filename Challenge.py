5e8620daa25e8375b9be936f9979f6cb25bb5979
Ahmed Fakhfakh <ahmed.fakhfakh@etu.univ-nantes.fr>
2024-01-16 11:40:18 +0100

    Commenter la classe Challenge

--- Challenge.py
+++ Challenge.py
@@ -1,6 +1,30 @@
+"""
+@title : Challenge
+@description : Class defining what a challenge is
+@author : El ARAJNA Lina , FAKHFAKH Ahmed , LALANNE-TISNE Nino, Madjibaye Donatien
+@date : Last Modification : 23/12/2023
+"""
+
 from utils.Drone import Drone
+
+
 class Challenge:
-    def __init__(self, rows_count, columns_count, drone_count,deadline,max_payload,product_weights,warehouses,orders):
+    """
+        Class is defined by:
+            - row_count
+            - columns_count
+            - drone_count
+            - deadline
+            - max_payload
+            - product_weights
+            - warehouses
+            - orders
+        """
+
+    """ Constructor """
+
+    def __init__(self, rows_count, columns_count, drone_count, deadline, max_payload, product_weights, warehouses,
+                 orders):
         self.rows_count = rows_count
         self.columns_count = columns_count
         self.drone_count = drone_count
@@ -11,5 +35,4 @@ class Challenge:
         self.orders = orders
         self.drones = []
         for i in range(drone_count):
-            self.drones.append(Drone(i,self.max_payload,self.warehouses[0].location))
-
+            self.drones.append(Drone(i, self.max_payload, self.warehouses[0].location))
