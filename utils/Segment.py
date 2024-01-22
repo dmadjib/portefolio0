8b560345cb7667018daa5a05a80f60ded52c765a
Nino Lalanne-Tisné <nino.lalanne-tisne@etu.univ-nantes.fr>
2024-01-22 20:49:03 +0100

    First version of workload repartition solver

--- utils/Segment.py
+++ utils/Segment.py
@@ -19,23 +19,24 @@ class Segment:
 
     """ Constructor """
 
-    def __init__(self, start, end, challenge, actions) -> None:
+    def __init__(self, start, end, challenge, actions, order_id, index) -> None:
+        self.order_id = order_id
         self.start = start
         self.end = end
         self.actions = actions
-        self.turns = self.calcul_turns(challenge)
+        self.turns = self.calcul_turns(challenge, index)
 
     """ Static Method """
     @staticmethod
-    def get_location(action, challenge):
+    def get_location(action, challenge, index):
         """
             - Get the location of the warehouse or the order
             :return:        The location of the warehouse if the action is 'L' or 'U',
                             the location of the order if the action is not 'L' or 'U'
         """
-        return challenge.warehouses[action[2]].location if action[1] in {'L', 'U'} else challenge.orders[action[2]].location
+        return challenge.warehouses[action[index]].location if action[index-1] in {'L', 'U'} else challenge.orders[action[index]].location
 
-    def calcul_turns(self, challenge):
+    def calcul_turns(self, challenge, index):
         """
             - Calculate the turns of a challenge
             :return:        The turns
@@ -46,7 +47,7 @@ class Segment:
 
         for action in self.actions:
             # Recuperate the location of the action
-            location = Segment.get_location(action, challenge)
+            location = Segment.get_location(action, challenge, index)
             # Add turns for movement (after deposit / charge)
             turns += Drone.calculate_distance(pos, location) + 1
             # Update the location of the drone
