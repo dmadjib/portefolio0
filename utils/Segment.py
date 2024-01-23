4ca2b5f25c9e698402adc1c34b419ea9ef858941
Ahmed Fakhfakh <ahmed.fakhfakh@etu.univ-nantes.fr>
2024-01-23 09:49:25 +0100

    Commenter la classe Segment

--- utils/Segment.py
+++ utils/Segment.py
@@ -1,7 +1,23 @@
+"""
+@title : Segment
+@description : Class defining what a segment is
+@author : El ARAJNA Lina , FAKHFAKH Ahmed , LALANNE-TISNE Nino, Madjibaye Donatien
+@date : Last Modification : 20/01/2024
+"""
+
 from utils.Drone import Drone
 import math
 
 class Segment:
+    """
+        Class is defined by:
+            - start
+            - end
+            - challenge
+            - actions
+        """
+
+    """ Constructor """
 
     def __init__(self, start, end, challenge, actions) -> None:
         self.start = start
@@ -9,21 +25,31 @@ class Segment:
         self.actions = actions
         self.turns = self.calcul_turns(challenge)
 
+    """ Static Method """
     @staticmethod
     def get_location(action, challenge):
+        """
+            - Get the location of the warehouse or the order
+            :return:        The location of the warehouse if the action is 'L' or 'U',
+                            the location of the order if the action is not 'L' or 'U'
+        """
         return challenge.warehouses[action[2]].location if action[1] in {'L', 'U'} else challenge.orders[action[2]].location
 
     def calcul_turns(self, challenge):
+        """
+            - Calculate the turns of a challenge
+            :return:        The turns
+        """
         turns = 0
 
         pos = self.start
 
         for action in self.actions:
-            # Récupérer la localisation de l'action
+            # Recuperate the location of the action
             location = Segment.get_location(action, challenge)
-            # Ajout des tours pour le déplacement (après dépôt / charge)
+            # Add turns for movement (after deposit / charge)
             turns += Drone.calculate_distance(pos, location) + 1
-            # Mise à jour de la localisation du drone
+            # Update the location of the drone
             pos = location
 
         return turns
